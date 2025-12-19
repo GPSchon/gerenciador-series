54250280c45f2c1ebf5d9813937471841deb12f7
GPSchon <gps.schon@gmail.com>
2025-12-19 16:59:17 -0300

    incluindo autenticação

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Models\Season;
 use App\Models\Series;
 use App\Models\Episode;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SeriesController;
 
@@ -12,51 +13,65 @@ Route::name('api.')->group(function () {
         return $request->user();
     });
 
-    Route::apiResource('series', SeriesController::class);
-
-    Route::get('series/{id}/seasons', function (int $id){
-        $series = Series::find($id);
-        if($series === null){
-            return response()->json(['message' => 'Serie não encontrada'], 404);
-        }
-        return response()->json($series->Seasons, 200);
-    })->name('series.seasons');
-
-    Route::get('series/{id}/episodes', function (int $id){
-        $series = Series::find($id);
-        if($series === null){
-            return response()->json(['message' => 'Serie não encontrada'], 404);
-        }
-        return response()->json($series->Episodes, 200);
-    })->name('series.episodes');
-
-    Route::patch('episodes/{episode}/watch', function (Episode $episode, Request $request){
-        $episode->watched = $request->watched;
-        $episode->save();
-
-        return response()->json($episode, 200);
-    })->name('episode.watch');
-
-    Route::patch('season/{season}/episodes/watch', function (Season $season, Request $request){
-        $episodes = $season->episodes()
-        ->whereIn('number', $request->number)
-        ->get();
-
-        $toTrue = $episodes
-            ->where('watched', false)
-            ->pluck('id')
-            ->toArray();
-
-        Episode::whereIn('id', $episodes->pluck('id'))
-            ->update(['watched' => false]);
-
-        if (!empty($toTrue)) {
-            Episode::whereIn('id', $toTrue)
-                ->update(['watched' => true]);
+    Route::post('login', function(Request $request){
+        $credentials = $request->only('email','password');
+        if(!Auth::attempt($credentials)){
+            return response()->json('Verifique o usuário e a senha!', 401);
         }
 
-        return response()->json([
-            'updated' => $episodes->pluck('number'),
-        ], 200);
-    })->name('season.episode.watch');
+        $user = Auth::user();
+        $user->tokens()->delete();
+        $token = $user->createToken('token');
+        return $token->plainTextToken();
+    });
+
+    Route::middleware('auth:sanctum')->group(function (){
+        Route::apiResource('series', SeriesController::class);
+
+        Route::get('series/{id}/seasons', function (int $id){
+            $series = Series::find($id);
+            if($series === null){
+                return response()->json(['message' => 'Serie não encontrada'], 404);
+            }
+            return response()->json($series->Seasons, 200);
+        })->name('series.seasons');
+
+        Route::get('series/{id}/episodes', function (int $id){
+            $series = Series::find($id);
+            if($series === null){
+                return response()->json(['message' => 'Serie não encontrada'], 404);
+            }
+            return response()->json($series->Episodes, 200);
+        })->name('series.episodes');
+
+        Route::patch('episodes/{episode}/watch', function (Episode $episode, Request $request){
+            $episode->watched = $request->watched;
+            $episode->save();
+
+            return response()->json($episode, 200);
+        })->name('episode.watch');
+
+        Route::patch('season/{season}/episodes/watch', function (Season $season, Request $request){
+            $episodes = $season->episodes()
+                ->whereIn('number', $request->number)
+                ->get();
+
+            $toTrue = $episodes
+                ->where('watched', false)
+                ->pluck('id')
+                ->toArray();
+
+            Episode::whereIn('id', $episodes->pluck('id'))
+                ->update(['watched' => false]);
+
+            if (!empty($toTrue)) {
+                Episode::whereIn('id', $toTrue)
+                    ->update(['watched' => true]);
+            }
+
+            return response()->json([
+                'updated' => $episodes->pluck('number'),
+            ], 200);
+        })->name('season.episode.watch');
+    });
 });
